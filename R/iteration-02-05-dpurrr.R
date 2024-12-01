27b71c5fc1349a7c40b2f234a8c0a7f9c6feae3d
Ian Lyttle <ijlyttle@me.com>
2024-11-30 19:06:38 -0600

    redo filter for penguins

--- R/iteration-02-05-dpurrr.R
+++ R/iteration-02-05-dpurrr.R
@@ -1,32 +1,44 @@
 ## dplyr using purrr (if time permits)
 
 library("conflicted")
+library("palmerpenguins")
+library("dplyr")
 library("purrr")
 
-# start with a couple of helpers
+# simplify penguins (Sorry Allison!)
+penguins_local <-
+  penguins |>
+  mutate(across(where(is.factor), as.character)) |> # use strings, not factors
+  select(species, island, body_mass_g, sex) |>      # fewer columns
+  print()
+
+#' @param .data  data frame or tibble
+#' @return unnamed list of named lists
 dpurrr_to_list <- function(.data) {
   .data |>
     as.list() |>
     purrr::list_transpose(simplify = FALSE)
 }
 
+#' @param .x  unnamed list of named lists
+#' @return tibble
 dpurrr_to_tibble <- function(.x) {
   .x |>
     purrr::list_transpose() |>
     tibble::as_tibble()
 }
 
-# try out our helpers
-mtcars |>
-  head(1) |>
+# experiment with helpers
+penguins_local |>
+  head(2) |>
   dpurrr_to_list() |>
-  dpurrr_to_tibble() |>
-  print()
+  # dpurrr_to_tibble() |>
+  str()
 
 # filter can be a purrr::keep()
-mtcars |>
+penguins_local |>
   dpurrr_to_list() |>
-  keep(\(d) d$gear == 3) |>
+  keep(\(d) d$sex == "female" && !is.na(d$sex)) |>
   dpurrr_to_tibble() |>
   print()
 
@@ -35,9 +47,9 @@ dpurrr_mutate <- function(.x, mapper) {
   .x |> purrr::map(\(d) modifyList(d, mapper(d)))
 }
 
-mtcars |>
+penguins |>
   dpurrr_to_list() |>
-  dpurrr_mutate(\(d) list(wt_kg = d$wt * 1000 / 2.2)) |>
+  dpurrr_mutate(\(d) list(body_mass_kg = d$body_mass_g / 1000)) |>
   dpurrr_to_tibble() |>
   print()
 
@@ -46,40 +58,40 @@ dpurrr_summarise <- function(.x, reducer) {
   .x |> purrr::reduce(reducer) |> list()
 }
 
-mtcars |>
+penguins |>
   dpurrr_to_list() |>
   dpurrr_summarise(
     \(acc, d) list(
-      wt_min = min(acc$wt_min, d$wt),
-      wt_max = max(acc$wt_max, d$wt)
+      body_mass_g_min = min(acc$body_mass_g_min, d$body_mass_g, na.rm = TRUE),
+      body_mass_g_max = max(acc$body_mass_g_max, d$body_mass_g, na.rm = TRUE)
     )
   ) |>
   dpurrr_to_tibble() |>
   print()
 
 # extract the reducer-function to make things more concise
-wt_min_max <- function(acc, d) {
+body_mass_g_min_max <- function(acc, d) {
   list(
-    wt_min = min(acc$wt_min, d$wt),
-    wt_max = max(acc$wt_max, d$wt)
+    body_mass_g_min = min(acc$body_mass_g_min, d$body_mass_g, na.rm = TRUE),
+    body_mass_g_max = max(acc$body_mass_g_max, d$body_mass_g, na.rm = TRUE)
   )
 }
 
-mtcars |>
+penguins |>
   dpurrr_to_list() |>
-  dpurrr_summarise(wt_min_max) |>
+  dpurrr_summarise(body_mass_g_min_max) |>
   dpurrr_to_tibble() |>
   print()
 
 # add in a group-by
-mtcars |>
-  split(mtcars$gear) |>
+penguins |>
+  split(penguins$species) |>
   imap(
     function(.data, name) {
       .data |>
         dpurrr_to_list() |>
-        dpurrr_summarise(wt_min_max) |>
-        dpurrr_mutate(\(d) list(gear = as.integer(name))) |>
+        dpurrr_summarise(body_mass_g_min_max) |>
+        dpurrr_mutate(\(d) list(species = name)) |>
         dpurrr_to_tibble()
     }
   ) |>
